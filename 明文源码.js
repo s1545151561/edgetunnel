1cf637ae67796fcc0806ff35797d3af6ad8f0967
PaperCups <102964724+PaperCupsRay@users.noreply.github.com>
2024-12-09 18:25:17 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -48,6 +48,37 @@ let 动态UUID;
 export default {
 	async fetch(request, env, ctx) {
 		try {
+			async function getConfig(url) {
+				let newapi = null;
+				try {
+				const response = await fetch(url, {
+					method: 'GET',
+					headers: {
+						'Cache-Control': 'no-cache'//不缓存
+					}
+				});
+				let jsondata = await response.json();
+				//gitee内容
+				if (url.startsWith("https://gitee.com/api/v5/repos/")) {
+					newapi = JSON.parse(atob(jsondata.content));
+				} else {
+					newapi = jsondata;
+				}
+				} catch (error) {
+					newapi = error;
+				}
+				return newapi;
+			}
+			
+			if (env.CONFIG) {
+			    let config = await getConfig(env.CONFIG);
+			    if (config != null && typeof (config) == "object") {
+				for (let k in config) {
+				    env[k] = config[k];
+				}
+			    }
+			}
+			
 			const UA = request.headers.get('User-Agent') || 'null';
 			const userAgent = UA.toLowerCase();
 			userID = env.UUID || env.uuid || env.PASSWORD || env.pswd || userID;
